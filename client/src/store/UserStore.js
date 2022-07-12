f958ab519cfc0f85a42baae9a3382895e6b1599f
mrbomberman <mrbomberman99@gmail.com>
2022-07-12 18:15:39 +0300

    added store for user info

---
+++ client/src/store/UserStore.js
@@ -0,0 +1,25 @@
+import {makeAutoObservable} from 'mobx';
+
+export default class UserStore {
+    constructor() {
+        this._isAuth = false; // _ эта переменная изменятся не может
+        this._user = {};
+        makeAutoObservable(this); // теперь mobx будет следить за изменениями нашего конструктора
+    }
+    
+    setIsAuth(bool) {
+        this._isAuth = bool
+    }
+
+    setUser(user){ // действие для изменения пользователя 
+        this._user = user;
+    }
+    // вызывается только в случае, когда переменная, которая используется внутри была изменена
+    get isAuth() { // для получения переменных из нашего состояния
+        return this._isAuth;
+    }
+
+    get user() { // для получения переменных из нашего состояния
+        return this._user;
+    }
+}
