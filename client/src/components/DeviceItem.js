8240d7b6ca7ee9ab68b0cc98120d776afe21c889
mrbomberman <mrbomberman99@gmail.com>
2022-07-15 12:43:55 +0300

    added necessary files for device store and device components

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Col, Image } from 'react-bootstrap';
+import star from '../assets/star.png';
+import {useNavigate} from 'react-router-dom';
+import { DEVICE_ROUTE } from '../utils/consts';
+
+const DeviceItem = ({device}) => {
+
+    const navigate = useNavigate(); // с помощью него можно динамически передвигаться по странице
+    console.log(navigate)
+
+    return (
+        <Col md={3} className='mt-3'>
+            <Card style={{width: 150, cursor: 'pointer'}} border={'light'} 
+            onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
+                <Image width={150} height={150} src={device.img}/>
+                <div className='mt-2 d-flex justify-content-between align-items-center'>
+                    <div className='text-black-50'>Iphone...</div>
+                    <div className='d-flex align-items-center' >
+                        <div>{device.rating}</div>
+                        <Image width={15} height={15} src={star}/>
+                    </div>
+                </div>
+                <div>{device.name}</div>
+            </Card>
+        </Col>
+    );
+};
+
+export default DeviceItem;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,20 +2,22 @@ import {makeAutoObservable} from 'mobx';
 
 export default class DeviceStore {
     constructor() {
-        this._types = [
-            {id: 1, name: 'Холодильник'},
-            {id: 2, name: 'Смартфоны'}
-        ]
-        this._brands = [
-            {id: 1, name: 'Samsung'},
-            {id: 2, name: 'Apple'}
-        ]
+        this._types = []
+        this._brands = []
         this._devices = [
             {id: 1, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
             {id: 2, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
             {id: 3, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
-            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'}
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+            {id: 4, name: 'IPHONE 12', price: 60000, rating: 4, img: 'https://images.unsplash.com/photo-1611791485440-24e8239a0377?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwMTIlMjBwcm98ZW58MHx8MHx8&w=1000&q=80'},
+
         ]
+        this._selectedType = {};
+        this._selectedBrand = {};
         makeAutoObservable(this); // теперь mobx будет следить за изменениями нашего конструктора
     }
     
@@ -29,6 +31,13 @@ export default class DeviceStore {
     setDevices(devices){ // действие для изменения пользователя 
         this._devices = devices;
     }
+    // при нажатии на какой-то конкретный тип, нам надо будет его выделять
+    setSelectedType(type){
+        this._selectedType = type;
+    }
+    setSelectedBrand(brand){
+        this._selectedBrand = brand;
+    }
     // вызывается только в случае, когда переменная, которая используется внутри была изменена
     get types() { // для получения переменных из нашего состояния
         return this._types;
@@ -39,5 +48,13 @@ export default class DeviceStore {
     }
     get devices() { // для получения переменных из нашего состояния
         return this._devices;
+    }
+
+    get selectedType() {
+        return this._selectedType;
+    }
+    
+    get selectedBrand(){
+        return this._selectedBrand;
     }
 }
